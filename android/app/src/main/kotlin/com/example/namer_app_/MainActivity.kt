09ff38a2a14a144d57ee0f647b613247431f5120
Your Name <Muhammd Bilal>
2024-04-02 00:10:24 +0500

    First Commit

---
+++ android/app/src/main/kotlin/com/example/namer_app_/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.namer_app_
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+int days:30;
+String name:
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Material(
+          child: Container(
+            child: Text('Welcome to 30 days of flutter'),
+          ),
+        ),
+      ),
+    );
+  }
+}
