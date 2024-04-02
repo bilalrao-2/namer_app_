0c06beb3b0d15f15736eb08e26c071c172f006df
Bilal Rao <muhammadbilalrao.2@gmail.com>
2024-04-02 10:47:40 +0500

    changes done

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-int days:30;
-String name:
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Material(
-          child: Container(
-            child: Text('Welcome to 30 days of flutter'),
-          ),
-        ),
+      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
       ),
     );
   }
